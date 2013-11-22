6bbb3f2ac6d6eed1e778c2af168ad07760d82d4e
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2013-11-22 07:35:58 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AzPollerApp::Application.config.secret_key_base = '4dc1ef137191f12969bc200273ea2030e12d3895b1c7a2068585c862d1f726137ee018af0e4ebeccc7dc744eea2fb40e6d45ebb1c0c6667338779f6e2c806872'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AzPollerApp::Application.config.session_store :cookie_store, key: '_az_poller_app_session'
