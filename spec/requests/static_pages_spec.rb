afa272d623eaefdb9975611a3351b72305819993
JF Plante <jf.plante@hp.com>
2013-12-28 16:44:07 -0800

    Add title to page, more spec. Finishes chapter 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  let(:base_title)  {"APP"}
+  
   describe "Home page" do
     it "should have the content 'Amazon price poller'" do
       visit '/static_pages/home'
       expect(page).to have_content('Amazon price poller')
+    end
+    it "should have the title 'APP | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   
@@ -12,6 +19,10 @@ describe "Static pages" do
     it "should have the content 'help'" do
       visit '/static_pages/help'
       expect(page).to have_content("Help")
+    end
+    it "should have the title 'APP | Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 end
