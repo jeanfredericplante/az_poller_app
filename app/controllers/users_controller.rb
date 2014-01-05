bf29bdd7d6485a06aa1bd18e3c691d72ef62e4e6
JF Plante <jf.plante@hp.com>
2014-01-04 23:00:26 -0800

    Forces redirect to user's page when going to signup and signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-  def new
-  end
-  
+
   def show
     @user = User.find(params[:id])
   end
   
   def new
+    redirect_to user_path(current_user) if signed_in?  
     @user = User.new
   end
   
